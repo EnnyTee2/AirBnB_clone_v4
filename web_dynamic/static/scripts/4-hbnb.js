53379b5637644053e28d5f2cd0d1887a87ed3b29
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-28 03:41:27 +0100

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -20,19 +20,11 @@ $(document).ready(function () {
 	    }
 	    return (amenities.join(', '));
 	});
-    });
-});
 
 
-const apiStatus = $('DIV#api_status');
-$.ajax('http://0.0.0.0:5001/api/v1/places_search/').done(function (data) {
-    if (data.status === 'OK') {
-      apiStatus.addClass('available');
-    } else {
-      apiStatus.removeClass('available');
-    }
-  });
-  });
+
+
+$('button').click(function(){
 
 $.ajax({
     type: 'POST',
@@ -44,4 +36,7 @@ $.ajax({
 	    $('.places').append('<article> <div class="title"> <h2>' + currentPlace.name + '</h2><div class="price_by_night">' + '$' + currentPlace.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + currentPlace.max_guest + ' Guests</div><div class="number_rooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_rooms + ' Bedrooms</div><div class="number_bathrooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_bathrooms + ' Bathroom </div></div> <div class="user"></div><div class="description">' + '$' + currentPlace.description + '</div></article>');
 	}
     }
+});
+
+});
 });
